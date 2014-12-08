889533302ac21ee76bf99089ef7f885c2319349c
F. Sebile <fatimasebiletezcan@gmail.com>
2014-12-08 22:28:00 +0200

    admin interface added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Media, Author, Publisher
 # Register your models here.
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    pass
