44cd8bbad200dc2857ff66150faaf48c759eedd0
F. Sebile <fatimasebiletezcan@gmail.com>
2014-12-08 21:23:01 +0200

    Models created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+MEDIA_CHOICES = [
+    ("b", "Book"),
+    ("m", "Magazine"),
+    ("n", "Newspaper"),
+]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=160)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=160)
+
+
+class Media(models.Model):
+    isbn = models.BigIntegerField()
+    title = models.CharField(max_length=160)
+    year = models.DateField()
+    publisher = models.ForeignKey(Publisher)
+    author = models.ForeignKey(Author)
+    cover_image = models.URLField(blank=True, null=True)
+    type = models.CharField(max_length=1, choices=MEDIA_CHOICES)
