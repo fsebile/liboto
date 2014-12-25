82d836244551557c7d98521bbc403e593ae14c68
F. Sebile <fatimasebiletezcan@gmail.com>
2014-12-25 17:49:47 +0200

    Missing user model admin!

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Media, Author, Publisher, Transaction
+from django.contrib.auth.admin import UserAdmin
+from .models import Media, Author, Publisher, Transaction, User
 # Register your models here.
 
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(CustomUserAdmin, self).get_fieldsets(request, obj)
+        if obj:
+            fieldsets += (
+                ("Liboto", {'fields': ('book_limit', 'favorite_medias')}),
+            )
+        return fieldsets
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
