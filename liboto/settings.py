2827dca73cc1f23a7435a27189f80af8cc2b7f61
F. Sebile <fatimasebiletezcan@gmail.com>
2014-12-25 17:45:53 +0200

    this breaks pycharm :(

--- liboto/settings.py
+++ liboto/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = global_settings.INSTALLED_APPS + (
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
